7ede6e93a5eed1c6d5957f88e55149fac86dd0bc
Derajatul <derajatul@gmail.com>
2023-05-24 13:32:56 +0700

    change website name

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ const Navbar = () => {
   return (
     <div className='navbar bg-base-200 rounded-b-xl'>
       <div className='flex-1'>
-        <a className='btn-ghost btn text-3xl normal-case' href="/"><span className="text-primary">Masakan </span> Rumah</a>
+        <a className='btn-ghost btn text-3xl normal-case' href="/"><span className="text-primary">Hidangan </span> Internasional</a>
       </div>
       <div className='flex-none'>
         <ul className='menu menu-horizontal px-1'>
