d1ce782c1d48669282566a8f8e5bb623a0a1f8c1
Derajatul <derajatul@gmail.com>
2023-05-31 07:45:16 +0700

    update navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,23 @@
 const Navbar = () => {
   return (
-    <div className='navbar bg-base-200 rounded-b-xl'>
+    <div className='navbar rounded-b-xl'>
       <div className='flex-1'>
-        <a className='btn-ghost btn text-3xl normal-case' href="/"><span className="text-primary">Hidangan </span> Internasional</a>
+        <a className='btn-ghost btn' href='/'>
+         <p className="text-xl font-bold">Hidangan Internasional</p> 
+        </a>
       </div>
       <div className='flex-none'>
         <ul className='menu menu-horizontal px-1'>
           <li>
-            <a href="/">Home</a>
+            <a href='/' className="text-base font-medium">Home</a>
           </li>
-        
+
           <li>
-            <a href="/resep">Search</a>
+            <a href='/about' className="text-base font-medium">About</a>
+          </li>
+
+          <li>
+            <a href='/resep' className="text-base font-medium">Resep</a>
           </li>
         </ul>
       </div>
@@ -19,4 +25,4 @@ const Navbar = () => {
   );
 };
 
-export default Navbar
+export default Navbar;
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,20 +8,32 @@ export default {
       padding: {
         DEFAULT: '1rem',
         sm: '2rem',
-        lg: '4rem',
-        xl: '5rem',
-        '2xl': '6rem',
+        lg: '9rem',
+        xl: '10rem',
+        '2xl': '11rem',
       },
     },
   },
   plugins: [require('daisyui')],
   daisyui: {
     styled: true,
-    themes: true,
+    themes: [
+      {
+        myTheme: {
+          primary: "#72C269",
+          secondary: "#f6d860",
+          accent: "#37cdbe",
+          neutral: "#3d4451",
+          "base-100": "#FFFFFF"
+        }
+      },
+      true
+    ],
     base: true,
     utils: true,
     logs: true,
     rtl: false,
     prefix: '',
+    darkTheme: 'light',
   },
 };
