97bffee58c8f2980777c9746e1863d0002230c5a
Derajatul <derajatul@gmail.com>
2023-06-02 10:57:40 +0700

    fix pwa

--- src/main.jsx
+++ src/main.jsx
@@ -4,19 +4,6 @@ import App from './App.jsx';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker
-      .register('/sw.js')
-      .then((registration) => {
-        console.log('Service Worker registered:', registration);
-      })
-      .catch((error) => {
-        console.log('Service Worker registration failed:', error);
-      });
-  });
-}
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
